e335251cf7842b38456bf8d0d970a2b802b8b1d6
Radha <86818441+DrW3RK@users.noreply.github.com>
2022-07-08 14:51:03 +0200

    remove folders

--- build/assets/js/814f3328.ebb6e1a8.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkgrants=self.webpackChunkgrants||[]).push([[535],{5641:s=>{s.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"W3F grants blog","permalink":"/grants/blog/W3F grants blog"}]}')}}]);
