31c0c2e4a44d6e9b65e1dc663014733eef9015c2
Radha <86818441+DrW3RK@users.noreply.github.com>
2022-07-08 14:35:35 +0200

    remove docusaurus build

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/radha/Documents/GitHub/grants/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/radha/Documents/GitHub/grants/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/radha/Documents/GitHub/grants/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/radha/Documents/GitHub/grants/src/css/custom.css"),
-];
